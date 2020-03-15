8f9562ea42593bf803ee07de41f83f54b1f806a4
= <awakeup@mail.ru>
2020-03-15 10:51:37 +0300

    more null-checking in RootController, +beginning of testing

--- src/main/java/spring/RootController.java
+++ src/main/java/spring/RootController.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
@@ -12,41 +13,63 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	@ResponseBody
 	public void index(@RequestBody Map<String, String> request, HttpServletResponse response) throws IOException {
+		Objects.requireNonNull(request);
+		Objects.requireNonNull(response);
+
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
+		PrintWriter writer = response.getWriter();
 
 		String value = request.get("request");
+		if (value == null) {
+			configureResponse(response, writer, HttpServletResponse.SC_BAD_GATEWAY,
+					"wrong json format: \"request\" expected\n");
+			return;
+		}
 		List<Integer> list = null;
 
 		try {
-			list = Arrays.stream(value.split(" ")).map(s -> Integer.valueOf(s)).collect(Collectors.toList());
+			list = Arrays.stream(value.split(" ")).filter(s -> !s.trim().equals("")).map(s -> Integer.valueOf(s))
+					.collect(Collectors.toList());
 		} catch (NumberFormatException ex) {
-			response.setStatus(502);
-			PrintWriter out = response.getWriter();
-			out.write("bad, very bad!");
-			out.flush();
+			configureResponse(response, writer, HttpServletResponse.SC_BAD_GATEWAY, "в запросе не все числа\n");
 			return;
 		}
 
 		String result = list != null ? list.stream().map(i -> ++i + "").reduce((s1, s2) -> s1 + " " + s2).orElse("")
 				: "";
-		response.setStatus(200);
-		PrintWriter out = response.getWriter();
-		out.write(makeJson("response", result));
-		out.flush();
+		configureResponse(response, writer, HttpServletResponse.SC_OK, makeJson("response", result));
 	}
 
+	/**
+	 * Makes a json string from key and value
+	 * 
+	 * @param key
+	 * @param value
+	 * @return {"key":"value"}
+	 */
 	static String makeJson(String key, String value) {
 		return new StringBuilder("{\"").append(key).append("\": \"").append(value).append("\"}").toString();
+	}
+
+	/**
+	 *
+	 * @param response
+	 * @param writer
+	 * @param status
+	 * @param message
+	 */
+	static void configureResponse(HttpServletResponse response, PrintWriter writer, int status, String message) {
+		response.setStatus(status);
+		writer.write(message);
+		writer.flush();
 	}
 
 }
---
+++ src/test/java/spring/RootControllerTest.java
@@ -0,0 +1,14 @@
+package spring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RootControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
